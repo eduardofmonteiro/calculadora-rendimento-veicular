b8c82943865d814df2a7b0e1657db5ba7059dd77
eduardofmonteiro <dudmont1@gmail.com>
2019-10-07 12:44:53 -0300

    master

---
+++ calculadora_rendimento_veicular/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calculadora_rendimento_veicular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de rendimento veicular(Km/L)!");
+
+            Console.WriteLine("Insira a distancia penrcorrida(KM): ");
+            string dist = Console.ReadLine();
+            decimal distd = Convert.ToDecimal(dist);
+
+            Console.WriteLine("Insira o consumo de combustivel(L)");
+            string cons = Console.ReadLine();
+            decimal consd = Convert.ToDecimal(cons);
+
+            decimal rend = 0;
+
+            rend =
+                distd / consd;
+
+            Console.WriteLine("O rendimento do seu veiculo foi de:"+rend+"KM/L!");
+        }
+    }
+}
