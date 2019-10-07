11fc4120a542a8a855bdc99c07591baca752ae9e
eduardofmonteiro <dudmont1@gmail.com>
2019-10-07 13:43:15 -0300

    english version

---
+++ vehicle_performance_calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calculadora_rendimento_veicular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vehicle Performance Calculator (Km / L)!");
+
+            Console.WriteLine("Enter the distance traveled (KM): ");
+            string dist = Console.ReadLine();
+            decimal distd = Convert.ToDecimal(dist);
+
+            Console.WriteLine("Enter fuel consumption (L)");
+            string cons = Console.ReadLine();
+            decimal consd = Convert.ToDecimal(cons);
+
+            decimal perf = 0;
+
+            perf =
+                distd / consd;
+
+            Console.WriteLine("The performance of your vehicle was: "+perf+"KM/L!");
+        }
+    }
+}
